7fa8e900ea906c7514d1351f45fc79ed9b3bac4a
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-15 13:12:17 +0330

    ✨ btn added

---
+++ countries/src/components/common/Btn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const primary = "bg-[#0A61C9] text-center p-[10px] rounded-3xl text-amber-50"
+const secondary = "bg-[#BFB7B6] text-center text-[#737070] rounded-3xl p-[10px]"
+
+export default function Btn({content, onClick, type, className={primary}}) {
+    return (
+        <div>
+            <button type={type} onClick={onClick} className={className}>
+                {content}
+            </button>
+        </div>
+)}
--- countries/src/routes.jsx
+++ countries/src/routes.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router";
-import Home from "./pages/home";
+import Home from "./components/pages/home";
 
 
 const router = createBrowserRouter([
