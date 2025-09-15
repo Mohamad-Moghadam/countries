91d0304cefc18ea169e1bcb243b0eaf5853a5e59
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-15 12:31:48 +0330

    ✨ routes added

---
+++ countries/src/routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import Home from "./pages/home";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    }
+])
+
+export default router;
