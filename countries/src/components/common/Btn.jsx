f6fce043f403b1fad2ac35ab317afa7c8030fed6
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-16 12:50:16 +0330

    🐛 tailwind config fixed

--- countries/src/components/common/Btn.jsx
+++ countries/src/components/common/Btn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const primary = "bg-[#0A61C9] text-center p-[10px] rounded-3xl text-amber-50"
-const secondary = "bg-[#BFB7B6] text-center text-[#737070] rounded-3xl p-[10px]"
+const primary = "bg-[#0A61C9] text-center p-2.5 rounded-3xl text-amber-50"
+const secondary = "bg-[#BFB7B6] text-center text-[#737070] rounded-3xl p-2.5"
 
 export default function Btn({content, onClick, type, className={primary}}) {
     return (
