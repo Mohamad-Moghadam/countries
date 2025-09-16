60caddc9014aa49b071658f93bb797bbc5a61317
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-16 12:23:48 +0330

    🐛 navBar fixed

--- countries/src/components/shared/NavBar.jsx
+++ countries/src/components/shared/NavBar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link, Outlet } from 'react-router'
 
 export default function NavBar() {
     return (
-    <div className='felx justify-center items-center gap-[15px]'>
-        <Link to="/">Home</Link>
-        <Link to="/countries">Countries</Link>
-        <Link to="/about">About</Link>
-    </div>
+    <>
+        <div className='fixed top-0 left-0 w-full flex justify-center items-center gap-6 p-4 bg-gray-800 text-white shadow-md z-50'>
+            <Link to="/">Home</Link>
+            <Link to="/countries">Countries</Link>
+            <Link to="/about">About</Link>
+        </div>
+        <div className="pt-16">
+            <Outlet />
+        </div>
+    </>
 )}
