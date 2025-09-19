c7fc2986c4dcc7f8372d656186d2d478be955f9d
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-19 15:00:22 +0330

    🐛 nav bar ui fixed

--- countries/src/components/shared/NavBar.jsx
+++ countries/src/components/shared/NavBar.jsx
@@ -5,13 +5,14 @@ import Title from '../common/Title'
 export default function NavBar() {
     return (
     <>
-        <div className='fixed top-0 left-0 w-full flex justify-start items-center gap-6 p-4 bg-gray-800 text-white shadow-md z-50'>
-            <Title/> |
-            <Link to="/">Home</Link>
-            <Link to="/countries">Countries</Link>
-            <Link to="/about">About</Link>
+        <div className='fixed top-0 left-0 w-full flex flex-wrap md:flex-nowrap justify-start items-center gap-4 md:gap-6 p-4 md:p-6 bg-gray-800 text-white shadow-lg z-50'>
+            <Title/>
+            <span className="hidden md:inline text-gray-400">|</span>
+            <Link to="/" className='hover:text-blue-400 transition-colors duration-200'>Home</Link>
+            <Link to="/countries" className='hover:text-blue-400 transition-colors duration-200'>Countries</Link>
+            <Link to="/about" className='hover:text-blue-400 transition-colors duration-200'>About</Link>
         </div>
-        <div className="pt-16">
+        <div className="pt-20 md:pt-24">
             <Outlet />
         </div>
     </>
