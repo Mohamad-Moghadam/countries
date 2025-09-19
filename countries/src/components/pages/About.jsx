220aff4288a8df792e74b34c75fe9bfd673035e3
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-20 01:23:27 +0330

    🎉 about content added

--- countries/src/components/pages/About.jsx
+++ countries/src/components/pages/About.jsx
@@ -3,6 +3,17 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            <h1>about</h1>
+            <div>
+                <h1>About This Website</h1>
+            </div>
+            <span>
+                <p>
+                    This website uses the REST Countries API to display comprehensive information about countries from around the world.
+
+                    Explore our data to learn about country names, capitals, regions, populations, flags, and much more. Whether you’re curious about a particular country or looking for insights about global regions, our interactive explorer makes it easy.
+
+                    Our goal is to build a fully responsive and modern web application using the latest technologies, including React Router v7 for seamless routing and Tailwind CSS for a beautiful, responsive user interface.
+                </p>
+            </span>
         </div>
 )}
