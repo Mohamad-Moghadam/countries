793a2f290e37d7855a8596277ef287111a6955a0
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-19 15:35:36 +0330

    🐛 country card ui fixed

--- countries/src/components/common/CountryCard.jsx
+++ countries/src/components/common/CountryCard.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function CountryCard({country}) {
     return (
-        <div className='flex flex-col border-2 mt-2 mx-1 shadow-2xl text-black bg-[#1f807e]'>
-            <h2 className='ml-3 text-3xl'>
+        <div className='bg-white rounded-2xl shadow-md p-6 flex flex-col gap-2 hover:shadow-xl hover:translate-y-1 transition-transform transition-shadow duration-200'>
+            <h2 className='text-xl md:text-2xl font-bold text-gray-900'>
                 {country?.name?.common}
             </h2>
-            <h3 className='ml-3 text-2xl'>
+            <h3 className='text-md md:text-lg text-gray-700'>
                 {country?.region}
             </h3>
-            <p className='ml-3 text-2xl'>
-                population: {country.population}
+            <p className='text-sm md:text-base text-gray-600'>
+                population: {country.population.toLocaleString()}
             </p>
         </div>
 )}
