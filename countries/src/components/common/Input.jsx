a85804e536fe119f25b60447a37af9aba8ea6839
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-20 00:50:19 +0330

    🐛 search and filter ui fixed

--- countries/src/components/common/Input.jsx
+++ countries/src/components/common/Input.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Input({placeholder , register}) {
     return (
         <div className='w-full sm:w-6/12'>
-            <input type="text" placeholder={placeholder} {...register} className='w-full px-4 py-2.5 rounded-lg border border-gray-300 bg-white text-gray-700 placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition-all duration-200'/>
+            <input type="text" placeholder={placeholder} {...register} className='text-2xl h-12 w-full px-4 py-2.5 rounded-lg border border-gray-300 bg-white text-gray-700 placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition-all duration-200'/>
         </div>
 )}
--- countries/src/components/common/Select.jsx
+++ countries/src/components/common/Select.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 export default function Select({region, setRegion}) {
     return (
-        <div className='w-6/12 h-6'>
-            <select value={region} onChange={(e) => setRegion(e.target.value)} className='w-full h-full'>
-                <option>
+        <div className='w-6/12'>
+            <select value={region} onChange={(e) => setRegion(e.target.value)} className='w-full px-4 py-2.5 rounded-lg border border-gray-300 bg-white text-gray-700focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500transition-all duration-200 cursor-pointer text-black h-12'>
+                <option className='text-black'>
                     All Regions
                 </option>
-                <option>
+                <option className='text-black'>
                     Africa
                 </option>
-                <option>
+                <option className='text-black'>
                     Americas
                 </option>
-                <option>
+                <option className='text-black'>
                     Asia
                 </option>
-                <option>
+                <option className='text-black'>
                     Europe
                 </option>
-                <option>
+                <option className='text-black'>
                     Oceania
                 </option>
             </select>
